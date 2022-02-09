a9a14d9f62089c29011d28cca64f90659d7c26ce
Ethan Silvas <ethansilvas2@gmail.com>
2022-02-08 17:22:13 -0800

    rename variables; setup bank

--- casino.py
+++ casino.py
@@ -1,7 +1,18 @@
 import random
+import json
 from discord.ext import commands
 
 import constants
+
+class Bank(commands.Cog):
+    def __init__(self, bot, records=None):
+        self.bot = bot
+        self.records = {}
+
+    @commands.command()
+    async def money(self, ctx):
+        print(ctx.message.author)
+
 
 class Gambling(commands.Cog):
     def __init__(self, bot):
@@ -10,6 +21,11 @@ class Gambling(commands.Cog):
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
         print(reaction, user)
+        print(reaction.message.content)
+
+        if (reaction.message.author == self.bot.user):
+            print('wow this worked')
+            print(reaction.message.reactions)
 
     @commands.command()
     async def roll(self, ctx, *args):
@@ -17,40 +33,39 @@ class Gambling(commands.Cog):
 
         TODO: for now this is coded to be open for more than just 0-100 and 0-n rolling but maybe more functionalities aren't needed
         """
-        print(ctx.message.content)
-        commandAuthor = ctx.message.author
+        command_author = ctx.message.author
 
         if (args):
             if (len(args) == 1):
                 try: 
-                    maxRoll = int(args[0])
-                    await ctx.send(f'{commandAuthor.mention} rolled: {random.randint(0, maxRoll)}')
+                    max_roll = int(args[0])
+                    await ctx.send(f'{command_author.mention} rolled: {random.randint(0, max_roll)}')
                 except ValueError:
-                    await ctx.send(f'{commandAuthor.mention} {constants.ROLL_VALUE_ERROR}')
+                    await ctx.send(f'{command_author.mention} {constants.ROLL_VALUE_ERROR}')
             else:
-                await ctx.send(f'{commandAuthor.mention} {constants.ROLL_ARGUMENT_ERROR}')
+                await ctx.send(f'{command_author.mention} {constants.ROLL_ARGUMENT_ERROR}')
         else: 
             print('no args')
-            await ctx.send(f'{commandAuthor.mention} rolled: {random.randint(0, 100)}')
+            await ctx.send(f'{command_author.mention} rolled: {random.randint(0, 100)}')
 
     @commands.command()
     async def bet(self, ctx, *, args):
         """Make bets with other users using the same concept of /roll"""
-        commandAuthor = ctx.message.author
-        argsSplit = args.split()
+        command_author = ctx.message.author
+        args_split = args.split()
 
-        if (len(argsSplit) <= 1):
+        if (len(args_split) <= 1):
             await ctx.send(constants.MISSING_REQUIRED_ARGUMENT_ERROR)
         else:
             try: 
-                maxRoll = int(argsSplit[0])
-                argsSplit.pop(0)
-                print(argsSplit)
+                max_roll = int(args_split[0])
+                args_split.pop(0)
+                print(args_split)
 
-                betParticipants = ' '.join(argsSplit)
+                bet_participants = ' '.join(args_split)
 
-                await ctx.send(f'{commandAuthor.mention} wants to bet ${maxRoll} against {betParticipants}')
+                await ctx.send(f'{command_author.mention} wants to bet ${max_roll} against {bet_participants}')
             except ValueError:
-                await ctx.send(f'{commandAuthor.mention} {constants.BET_VALUE_ERROR}')
+                await ctx.send(f'{command_author.mention} {constants.BET_VALUE_ERROR}')
 
 
--- gamble_bot.py
+++ gamble_bot.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 
 from discord.ext import commands
@@ -13,5 +14,21 @@ bot = commands.Bot(command_prefix='/')
 
 bot.add_cog(error_handler.ErrorHandler(bot))
 bot.add_cog(casino.Gambling(bot))
+
+"""
+TODO: could maybe put json file here and call the bank cog with it f
+
+try: 
+    records_file = open('records.json')
+    records = json.load(records_file)
+    print(records)
+except IOError:
+    print('Could not find Records file.')
+    bot.add_cog(casino.Bank(bot))
+except json.JSONDecodeError:
+    print('Empty records file')
+"""
+bot.add_cog(casino.Bank(bot))
+
 
 bot.run(token)
