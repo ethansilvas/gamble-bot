a098cabafc1fc77a219cbce54cf5365c77274a1a
Ethan Silvas <ethansilvas2@gmail.com>
2022-08-05 18:32:41 -0700

    reset money working

--- bank.py
+++ bank.py
@@ -26,18 +26,15 @@ class Bank(commands.Cog):
     def update_records(self):
         """Updates the local records.json file with whatever the current records object value is"""
         try:
-            with open('records.json', 'r+') as records_file:
+            with open('records.json', 'w') as records_file:
                 records_file.write(json.dumps(self.records))
         except: 
             print('Unable to update records.json')
     
-    @commands.command(name='InitServerBank')
+    @commands.command(name='initServerBank')
     async def init_server_bank(self, ctx):
         """Initializes the server bank by adding a constant-defined amount to each non-bot member of the guild"""
-        if ctx.message.author.name != 'eLou':
-            print(ctx.message.author.name)
-            print('not eLou')
-        else:
+        if ctx.message.author.name == 'eLou':
             if len(self.records) == 0:
                 guild = str(ctx.message.guild.id)
                 self.records[guild] = {}
@@ -53,6 +50,9 @@ class Bank(commands.Cog):
 
                 with open('records.json', 'w') as outfile:
                     json.dump(self.records, outfile)
+        else:
+            print(ctx.message.author.name)
+            print('not eLou')
 
     @commands.command()
     async def money(self, ctx):
@@ -68,9 +68,20 @@ class Bank(commands.Cog):
     
     def add_money(self, guild, user, amount):
         """Adds to a given user's money"""
-        print('updating money')
         self.records[guild][user]['money'] += amount
-    
+
+    @commands.command(name='resetMoney')
+    async def reset_money(self, ctx):
+        if ctx.message.author.name == 'eLou':
+            guild = str(ctx.guild.id)
+            
+            for user in self.records[guild]:
+                self.records[guild][user]['money'] = constants.INIT_MONEY_AMOUNT
+
+            self.update_records()
+        else: 
+            print('not eLou')
+        
     @commands.command()
     async def leaderboard(self, ctx): 
         """Prints out the current money of all members in the guild"""
